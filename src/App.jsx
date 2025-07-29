c4ed23407eb4c4b580e21fe83091a6ad07d655aa
Jaweds11 <2400031055@kluniversity.in>
2025-07-29 22:24:55 +0530

    Updated homepage text

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
       <h1>welcome to forntend class in klu</h1>
       <h2> Today's Topic is Introduction to React</h2>
       <h3> Today's Date is 23.07.25</h3>
-      <h4> JAWED</h4>
+      <h4> asha</h4>
       <Link to="/page1">Go To Page1</Link>
        <br/><br/><br/>
        <Link to="/page2">Go To Page2</Link>
