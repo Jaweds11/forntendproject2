e0789a9d46375bbf3028dc60103d9399e285597b
Jaweds11 <2400031055@kluniversity.in>
2025-07-29 15:38:02 +0530

    version1

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import Page1 from './Page1.jsx'
 import Page2 from './Page2.jsx'
 import Page3 from './Page3.jsx'
 createRoot(document.getElementById('root')).render(
-<BrowserRouter>
+<BrowserRouter basename="forntendproject2">
   <Routes>
     <Route path="/" element = {<App/>}></Route>
     <Route path='/Page1' element={<Page1/>}></Route>
